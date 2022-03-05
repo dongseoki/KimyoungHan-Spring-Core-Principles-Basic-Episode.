e17c79e61349431340353ca01a7cc4bd3f062b96
dongseoki <43495319+dongseoki@users.noreply.github.com>
2022-03-05 16:44:55 +0900

    김영한 강의 내용 커밋

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,22 +9,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
+
+    @Bean
     public MemberService memberService(){
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig MemberRepository");
         return new MemoryMemberRepository();
     }
-    private DiscountPolicy discountPolicy() {
+
+    @Bean
+    public DiscountPolicy discountPolicy() {
         return new FixDiscountPolicy();
 //        return new RateDiscountPolicy();
     }
 
-
+    @Bean
     public OrderService orderService(){
+        System.out.println("call AppConfig orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
---
+++ src/test/java/hello/core/pleaseuseconstructorinjection/UseConstructInjection.java
@@ -0,0 +1,21 @@
+package hello.core.pleaseuseconstructorinjection;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.Order;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UseConstructInjection {
+    @Test
+    void createOrder(){
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+        OrderServiceImpl orderService = new OrderServiceImpl(memberRepository, new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+//        Assertions
+    }
+}
